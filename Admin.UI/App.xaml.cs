0639a1d647cc005317f3b3dffd52789e94714153
Konstantin Burov <konstantin.burov@hotmail.com>
2012-11-02 08:31:14 +0200

    Added UnhandledException handler to Admin program

--- Admin.UI/App.xaml.cs
+++ Admin.UI/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 
 namespace Testing.Admin.UI
 {
@@ -9,10 +10,27 @@ namespace Testing.Admin.UI
     {
         private void ApplicationStartup(object sender, StartupEventArgs e)
         {
+            AppDomain.CurrentDomain.UnhandledException += UnhandledExceptionHandler;
             // Apply skin
             AppController.ApplySkin();
             // Load localized strings
             AppController.ApplyInterfaceLanguage();
+        }
+
+        private static void UnhandledExceptionHandler(object sender, UnhandledExceptionEventArgs e)
+        {
+            try
+            {
+                var ex = e.ExceptionObject as Exception;
+
+                if (ex != null)
+                    MessageBox.Show(
+                        "Please contact the developers with the following information:\n\n" + ex.Message + ex.StackTrace,
+                        "Fatal Error", MessageBoxButton.OK, MessageBoxImage.Stop);
+            }
+// ReSharper disable EmptyGeneralCatchClause
+            catch { }
+// ReSharper restore EmptyGeneralCatchClause
         }
     }
 }
